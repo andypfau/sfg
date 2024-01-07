5f225b19fe6f19b500d607c33363a09a17aff143
Andreas Pfau <andreas.pfau@apfau.de>
2024-01-07 16:34:19 +0100

    updated environment script

--- samples/_env.py
+++ samples/_env.py
@@ -1,4 +1,10 @@
+# The sample files are supposed to be called from the root folder of
+#   the repo. This script ensures the library folder is found, and
+#   also that an empty folder <output> is created for the generated
+#   files.
+
 import sys
 import os
 
 sys.path.insert(0, '.')
+os.makedirs('./output')
